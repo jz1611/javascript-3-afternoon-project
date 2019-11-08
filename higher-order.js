b13e1ef0f75d6ecbdcfcf751a6d48053ee2321ba
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-11-08 16:40:51 -0700

    Completed higher order problems, but confused on Problem 6

--- higher-order.js
+++ higher-order.js
@@ -20,10 +20,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
-
-
+let evenNumbers = mixedNumbers.filter((num) => {
+  return num % 2 === 0;
+})
 
 ////////// PROBLEM 2 //////////
 
@@ -43,10 +42,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
-
-
+let postTaxPrices = prices.map((price) => {
+  return price *= 1.07;
+});
 
 ////////// PROBLEM 3 //////////
 
@@ -63,9 +61,9 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
-
-
+let totalPopulation = populations.reduce((acc, pop) => {
+  return acc + pop;
+}, 0);
 
 ////////// PROBLEM 4 //////////
 
@@ -88,10 +86,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
   Use the filter method to return only the monsters that have a CP of over 200.
 */
 
-//Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
-
-
+let myStrongest = monstersInYourPocket.filter((obj) => {
+  return obj.CP > 200;
+})
 
 ////////// PROBLEM 5 //////////
 
@@ -106,9 +103,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax. Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
-
-
+let orderTotals = orders.map((obj) => {
+  return obj.price + obj.price * obj.tax;
+})
 
 ////////// PROBLEM 6 //////////
 
@@ -126,6 +123,8 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
-
-
+let bobsTotal = purchases.filter(function(obj){
+  return obj.owner === 'Bob';
+}).reduce(function(acc, obj){
+  return {price: acc.price + obj.price}
+}).price;
